a97cff5ecc277d3e08446d1ae8f29fc6a4409873
Igor Alaev <ialaev@ics.com>
2024-04-04 22:09:18 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+    app.setOrganizationName("ICS");
+    app.setApplicationName("TS file updater");
+    app.setApplicationVersion("1.0");
+
+
+}
