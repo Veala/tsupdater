00463141c0d6f8d8c61e6ba697b11f8fdb4849df
Igor Alaev <ialaev@ics.com>
2024-04-09 21:50:29 +0300

    finish command line parser

--- main.cpp
+++ main.cpp
@@ -5,31 +5,39 @@ int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
     app.setOrganizationName("ICS");
-    app.setApplicationName("TS file updater");
+    app.setApplicationName("TS updater");
     app.setApplicationVersion("1.0");
 
-
     QCommandLineParser parser;
-    parser.setApplicationDescription("Updates Linguist .ts files");
-    parser.process(app);
+    parser.setApplicationDescription("Updates Linguist \"ts\" files");
 
-
-    const QStringList opt = parser.optionNames();
-    QString locale;
-    bool usePigLatin = false;
-    bool success =false;
-
-    if (parser.isSet(localeOption)) {
-        locale = parser.value(localeOption);
-    } else  {
-        //Use PigLatin
-        usePigLatin = true;
+    QString errorMessage;
+    switch (parseCommandLine(parser, &errorMessage)) {
+    case CommandLineOk:
+        break;
+    case CommandLineError:
+        fputs(qPrintable(errorMessage), stderr);
+        fputs("\n\n", stderr);
+        return 1;
+    case CommandLineVersionRequested:
+        printf("%s %s\n", qPrintable(QCoreApplication::applicationName()),
+               qPrintable(QCoreApplication::applicationVersion()));
+        return 0;
+    case CommandLineHelpRequested:
+        parser.showHelp();
+        Q_UNREACHABLE();
     }
 
-
-    const QStringList args = parser.positionalArguments();
-    if (args.isEmpty()) {
-        parser.showHelp(1);
+    const QStringList srcAndTsPaths = parser.positionalArguments();
+    switch (updateTsFile(srcAndTsPaths, &errorMessage)) {
+    case UpdateOk:
+        printf("File %s is updated\n", qPrintable(srcAndTsPaths.last()));
+        return 0;
+    case UpdateError:
+        fputs(qPrintable(errorMessage), stderr);
+        fputs("\n\n", stderr);
+        return 1;
     }
+
     return 0;
 }
--- tsupdaterfeatures.h
+++ tsupdaterfeatures.h
@@ -2,6 +2,10 @@
 #define FEATURES_H
 
 #include <QCommandLineParser>
+#include <QFileInfo>
+#include <QList>
+
+namespace  {
 
 enum CommandLineParseResult
 {
@@ -11,12 +15,23 @@ enum CommandLineParseResult
     CommandLineHelpRequested
 };
 
+enum UpdateTsFileResult
+{
+    UpdateOk,
+    UpdateError
+};
+
+QStringList ValidSourceFileExtensions  = { "stf", "sdf" };
+
+}
+
 CommandLineParseResult parseCommandLine(QCommandLineParser &parser, QString *errorMessage)
 {
-    parser.addPositionalArgument("srcfile", QCoreApplication::translate("args", "Source translation file."));
-    parser.addPositionalArgument("tsfile", QCoreApplication::translate("args", "TS file to update."));
+    parser.addPositionalArgument("srcfile", QString("Source translation file (%1).").arg(ValidSourceFileExtensions.join(", ")));
+    parser.addPositionalArgument("tsfile", "TS file to update.");
 
-    const QCommandLineOption helpOption = parser.addHelpOption();
+    const QCommandLineOption helpOption(QStringList() << "h" << "help", "Displays help on commandline options.");
+    parser.addOption(helpOption);
     const QCommandLineOption versionOption = parser.addVersionOption();
 
     if (!parser.parse(QCoreApplication::arguments())) {
@@ -35,13 +50,44 @@ CommandLineParseResult parseCommandLine(QCommandLineParser &parser, QString *err
         *errorMessage = "No arguments";
         return CommandLineError;
     }
-    if (positionalArguments.size() > 1) {
-        *errorMessage = "Several 'name' arguments specified.";
+    if (positionalArguments.size() > 2) {
+        *errorMessage = "More than two arguments specified.";
         return CommandLineError;
     }
-    query->name = positionalArguments.first();
+    if (positionalArguments.size() < 2) {
+        *errorMessage = "Less than two arguments specified.";
+        return CommandLineError;
+    }
+
+    QFileInfo srcfileinfo(positionalArguments.first());
+    if (!srcfileinfo.exists()) {
+        *errorMessage = QString("File %1 is not exists.").arg(srcfileinfo.filePath());
+        return CommandLineError;
+    }
+    if (!ValidSourceFileExtensions.contains(srcfileinfo.suffix())) {
+        *errorMessage = QString("Source file \"%1\" extension is not supported.\n").arg(srcfileinfo.suffix()) +
+                QString("Source files with the extensions \"%1\" are processed.").arg(ValidSourceFileExtensions.join(", "));
+        return CommandLineError;
+    }
+
+    QFileInfo tsfileinfo(positionalArguments.last());
+    if (!tsfileinfo.exists()) {
+        *errorMessage = QString("File %1 is not exists.").arg(tsfileinfo.filePath());
+        return CommandLineError;
+    }
+    if (tsfileinfo.suffix() != "ts") {
+        *errorMessage = "File extension is not \"ts\": " + tsfileinfo.filePath();
+        return CommandLineError;
+    }
 
     return CommandLineOk;
+}
+
+UpdateTsFileResult updateTsFile(QStringList srcTsPaths, QString *errorMessage)
+{
+    QString srcFilePath = srcTsPaths.first();
+    QString tsFilePath = srcTsPaths.last();
+
 }
 
 #endif // FEATURES_H
